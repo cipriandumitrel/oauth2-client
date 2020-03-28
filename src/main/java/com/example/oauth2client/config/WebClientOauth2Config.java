178bba64b84011676dd715d9c9ab8cc9c7409d5e
Ciprian Dumitrel <ciprian.dumitrel@metrosystems.net>
2020-03-28 20:42:32 +0200

    Updated config

--- src/main/java/com/example/oauth2client/config/WebClientOauth2Config.java
+++ src/main/java/com/example/oauth2client/config/WebClientOauth2Config.java
@@ -4,6 +4,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.ClientCredentialsReactiveOAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.endpoint.WebClientReactiveClientCredentialsTokenResponseClient;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager;
@@ -23,7 +26,7 @@ public class WebClientOauth2Config {
     this.externalApiSettings = externalApiSettings;
   }
 
-  @Bean
+  /*@Bean
   WebClient webClient(ReactiveClientRegistrationRepository reactiveClientRegistrationRepository,
                       ServerOAuth2AuthorizedClientRepository serverOAuth2AuthorizedClientRepository) {
 
@@ -48,9 +51,35 @@ public class WebClientOauth2Config {
             .build();
 
     return client;
-    //return builder.baseUrl("http://localhost:8080")
-    //            .build();
+  }*/
 
+  @Bean
+  public WebClient webClient(ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+    ServerOAuth2AuthorizedClientExchangeFilterFunction oauth =
+            new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+    oauth.setDefaultClientRegistrationId(CLIENT_REGISTRATION_ID);
+    return WebClient.builder()
+            .baseUrl(externalApiSettings.getUri())
+            .filter(oauth)
+            .build();
+  }
 
+  @Bean
+  public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
+          ReactiveClientRegistrationRepository clientRegistrationRepository,
+          ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+    ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider =
+            ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                    .clientCredentials()
+                    .build();
+
+    DefaultReactiveOAuth2AuthorizedClientManager authorizedClientManager =
+            new DefaultReactiveOAuth2AuthorizedClientManager(
+                    clientRegistrationRepository, authorizedClientRepository);
+
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+    return authorizedClientManager;
   }
 }
